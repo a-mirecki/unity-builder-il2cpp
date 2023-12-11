de45cf340aada9c83d820134bb90b4596c148812
a-mirecki <a.mirecki99@gmail.com>
2023-12-11 13:54:52 +0100

    extra echo

--- dist/platforms/ubuntu/steps/create_upmconfig.sh
+++ dist/platforms/ubuntu/steps/create_upmconfig.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
+echo "Creating upmconfig.toml"
+
+
 echo "[npmAuth.\"https://npm.pkg.github.com/@rrealmdev\"]" > ~/.upmconfig.toml
 echo "token = \"$GIT_PRIVATE_TOKEN}\"" >> ~/.upmconfig.toml
 echo "email = \"arkadiusz.mirecki@everyrealm.com\"" >> ~/.upmconfig.toml
 echo "alwaysAuth = true" >> ~/.upmconfig.toml
 
+ls ~
+cat ~/.upmconfig.toml
